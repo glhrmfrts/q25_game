b3d46ead4c4e59ab0804b939541e64fd3b962265
Daniel Wolf <nephatrine@gmail.com>
2023-04-05 12:05:20 -0400

    remove reliance on def files as they are Windows-specific

--- missionpack/g_main.c
+++ missionpack/g_main.c
@@ -207,7 +207,7 @@ Returns a pointer to the structure with all entry points
 and global variables
 =================
 */
-game_export_t *GetGameAPI (game_import_t *import)
+GAMEAPI_EXPORT game_export_t *GetGameAPI (game_import_t *import)
 {
 	gi = *import;
 
